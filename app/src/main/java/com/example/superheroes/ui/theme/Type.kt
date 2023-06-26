fc7d203e570efce41b5febdef79ef50d88b042ad
CheoR <cheo.roman+github@gmail.com>
2023-06-26 08:00:34 -0500

    build: typography fonts

--- app/src/main/java/com/example/superheroes/ui/theme/Type.kt
+++ app/src/main/java/com/example/superheroes/ui/theme/Type.kt
@@ -2,18 +2,40 @@ package com.example.superheroes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.superheroes.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular),
+    Font(R.font.cabin_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
